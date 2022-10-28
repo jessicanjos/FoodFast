42bcadb98cfb5730a06f952b25acea753c2c68c9
jessicanjos <jessicanjos2003@gmail.com>
2022-10-27 22:13:17 -0300

    Criação menu

--- app/src/main/java/br/com/etecia/foodfast/MainActivity.java
+++ app/src/main/java/br/com/etecia/foodfast/MainActivity.java
@@ -1,8 +1,13 @@
 package br.com.etecia.foodfast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //inserindo o menu na barra do aplicativo
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //Criando o método
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mFav:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no favoritos",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mNovoGrupo:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no novo grupos",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mPagamentos:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no pagamentos",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mConfig:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no configurações",
+                        Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
